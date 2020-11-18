2ae615aab7b156b389fdb440314e3ef638bbebdb
Chris Staples <stapleschris0@gmail.com>
2020-11-17 19:25:29 -0500

    project file creation
    setup the project for VS2019

---
+++ Dream-Engine/Dream-Engine/main.cpp
@@ -0,0 +1,13 @@
+//Main source file for Dream-Engine
+
+#include <iostream>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	std::cin.get();
+}
