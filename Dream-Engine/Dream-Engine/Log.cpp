7333b16e3d795108d592fdadc65de6121fdfa394
Chris Staples <stapleschris0@gmail.com>
2020-12-01 14:47:46 -0500

    fix stuff
    clean project files

--- Dream-Engine/Dream-Engine/Log.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdio.h>
-
-#include <GLFW/glfw3.h>
-
-void error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error: %s\n", description);
-}
--- Dream-Engine/Dream-Engine/main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <GLFW/glfw3.h>
-
-void error_callback(int error, const char* description);
-
